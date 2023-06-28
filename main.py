8b257b32453eb1534ee14ac92f9ffce58eca08d6
faris771 <farisabufahra33@gmail.com>
2023-06-29 01:49:23 +0300

    K-NN is Done, i guess...

--- main.py
+++ main.py
@@ -1,16 +1,136 @@
-# This is a sample Python script.
+import numpy as np
+import csv
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
+from sklearn.neural_network import MLPClassifier
+
+# %%
+from sklearn.neighbors import KNeighborsClassifier
+
+TEST_SIZE = 0.3
+K = 3
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class NN:
+
+    def __init__(self, trainingFeatures, trainingLabels) -> None:
+        self.trainingFeatures = trainingFeatures
+        self.trainingLabels = trainingLabels
+
+    def predict(self, tesing_features, k):
+        classifier = KNeighborsClassifier(n_neighbors=k)
+        classifier.fit(self.trainingFeatures, self.trainingLabels)
+        label_prediction = classifier.predict(tesing_features)
+        return label_prediction
+
+        """
+        Given a list of features vectors of testing examples
+        return the predicted class labels (list of either 0s or 1s)
+        using the k nearest neighbors
+        """
+
+        raise NotImplementedError
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# %%
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def load_data(filename):
+    df = pd.read_csv(filename)  # data frame
+    """
+    Load spam data from a CSV file `filename` and convert into a list of
+    features vectors and a list of target labels. Return a tuple (features, labels).
+
+    features vectors should be a list of lists, where each list contains the
+    57 features vectors
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if spam, and 0 otherwise.
+    """
+
+    return df.iloc[:, :-1].values, df.iloc[:, -1].values
+
+
+# %%
+
+
+def preprocess(features):
+    """
+    normalize each feature by subtracting the mean value in each
+    feature and dividing by the standard deviation
+    """
+    raise NotImplementedError
+
+
+def train_mlp_model(features, labels):
+    """
+    Given a list of features lists and a list of labels, return a
+    fitted MLP model trained on the data using sklearn implementation.
+    """
+    raise NotImplementedError
+
+
+def evaluate(labels, predictions):
+    return accuracy_score(labels, predictions), precision_score(labels, predictions), recall_score(labels,
+                                                                                                   predictions), f1_score(
+        labels, predictions)
+
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return (accuracy, precision, recall, f1).
+
+    Assume each label is either a 1 (positive) or 0 (negative).
+    """
+    raise NotImplementedError
+
+
+# %%
+
+
+def main():
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python template.py ./spambase.csv")
+
+    # Load data from spreadsheet and split into train and test sets
+    features, labels = load_data(sys.argv[1])
+
+    # features = preprocess(features)  # MLP
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        features, labels, test_size=TEST_SIZE)
+
+    # Train a k-NN model and make predictions
+    model_nn = NN(X_train, y_train)
+    predictions = model_nn.predict(X_test, K)
+
+    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+
+    # Print results
+    print("**** 1-Nearest Neighbor Results ****")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1: ", f1)
+
+    """
+    
+    # Train an MLP model and make predictions
+    model = train_mlp_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+
+    # Print results
+    print("**** MLP Results ****")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1: ", f1)
+    """
+
+
+if __name__ == "__main__":
+    main()
