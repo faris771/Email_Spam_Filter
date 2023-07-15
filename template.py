e5d44a2cabb84199fdd6fc08cc6a1090b56145c9
faris771 <farisabufahra33@gmail.com>
2023-07-15 03:28:01 +0300

    modifications K-NN set to Euclidean Confusion matrix added

--- template.py
+++ template.py
@@ -1,7 +1,6 @@
 import numpy as np
 import csv
 import sys
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
@@ -20,10 +19,10 @@ class NN:
         self.trainingFeatures = trainingFeatures
         self.trainingLabels = trainingLabels
 
-    def predict(self, tesing_features, k):
-        classifier = KNeighborsClassifier(n_neighbors=k)
+    def predict(self, testing_features, k):
+        classifier = KNeighborsClassifier(n_neighbors=k, p=2)  # p=2 is euclidean distance
         classifier.fit(self.trainingFeatures, self.trainingLabels)
-        label_prediction = classifier.predict(tesing_features)
+        label_prediction = classifier.predict(testing_features)
         return label_prediction
 
         """
@@ -31,8 +30,6 @@ class NN:
         return the predicted class labels (list of either 0s or 1s)
         using the k nearest neighbors
         """
-
-        raise NotImplementedError
 
 
 # %%
@@ -51,7 +48,7 @@ def load_data(filename):
     is 1 if spam, and 0 otherwise.
     """
 
-    return df.iloc[:, :-1].values, df.iloc[:, -1].values
+    return df.iloc[:, :-1].values, df.iloc[:, -1].values  # features, labels
 
 
 # %%
@@ -62,7 +59,11 @@ def preprocess(features):
     normalize each feature by subtracting the mean value in each
     feature and dividing by the standard deviation
     """
-    raise NotImplementedError
+    features = np.array(features)
+    mean = np.mean(features, axis=0)
+    std = np.std(features, axis=0)
+    features = (features - mean) / std
+    return features.tolist()
 
 
 def train_mlp_model(features, labels):
@@ -70,21 +71,24 @@ def train_mlp_model(features, labels):
     Given a list of features lists and a list of labels, return a
     fitted MLP model trained on the data using sklearn implementation.
     """
-    raise NotImplementedError
+    model = MLPClassifier(random_state=1, hidden_layer_sizes=(10, 5), max_iter=2000, activation='logistic')
+    model.fit(features, labels)
+    return model
 
 
 def evaluate(labels, predictions):
-    return accuracy_score(labels, predictions), precision_score(labels, predictions), recall_score(labels,
-                                                                                                   predictions), f1_score(
-        labels, predictions)
-
     """
     Given a list of actual labels and a list of predicted labels,
-    return (accuracy, precision, recall, f1).
+    return (accuracy, precision, recall, f1). -we added confusion matrix-
 
     Assume each label is either a 1 (positive) or 0 (negative).
     """
-    raise NotImplementedError
+    accuracy = accuracy_score(labels, predictions)
+    precision = precision_score(labels, predictions)
+    recall = recall_score(labels, predictions)
+    f1 = f1_score(labels, predictions)
+    confusion_mtrx = confusion_matrix(labels, predictions)
+    return accuracy, precision, recall, f1, confusion_mtrx
 
 
 # %%
@@ -97,7 +101,6 @@ def main():
 
     # Load data from spreadsheet and split into train and test sets
     features, labels = load_data(sys.argv[1])
-
     # features = preprocess(features)  # MLP
 
     X_train, X_test, y_train, y_test = train_test_split(
@@ -106,22 +109,20 @@ def main():
     # Train a k-NN model and make predictions
     model_nn = NN(X_train, y_train)
     predictions = model_nn.predict(X_test, K)
-
-    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+    accuracy, precision, recall, f1, confusion_mtrx = evaluate(y_test, predictions)
 
     # Print results
-    print("**** 1-Nearest Neighbor Results ****")
+    print("**** k = 3 Nearest Neighbor Results ****")
     print("Accuracy: ", accuracy)
     print("Precision: ", precision)
     print("Recall: ", recall)
     print("F1: ", f1)
+    print("Confusion Matrix: \n", confusion_mtrx)
 
-    """
-    
     # Train an MLP model and make predictions
     model = train_mlp_model(X_train, y_train)
     predictions = model.predict(X_test)
-    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+    accuracy, precision, recall, f1, confusion_mtrx = evaluate(y_test, predictions)
 
     # Print results
     print("**** MLP Results ****")
@@ -129,7 +130,7 @@ def main():
     print("Precision: ", precision)
     print("Recall: ", recall)
     print("F1: ", f1)
-    """
+    print("Confusion Matrix:\n ", confusion_mtrx)
 
 
 if __name__ == "__main__":
